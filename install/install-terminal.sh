6b4ca19daac9157d5a0f3753e4cfe4aabb5c5259
Alan Champion <alanchampion@protonmail.com>
2022-06-26 18:40:20 -0500

    Fixing omf and starship

--- install/install-terminal.sh
+++ install/install-terminal.sh
@@ -2,6 +2,8 @@
 
 sudo apt install -y terminator
 sudo apt install -y fish
+# Oh My Fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 # Hack Font
 cd ~/Downloads
 wget -O "Hack Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
@@ -10,7 +12,11 @@ cp "Hack Nerd Font Complete.ttf" ~/.fonts
 fc-cache -fv
 cd -
 # Starship
+echo "Starting to install starship"
+read starship
 curl -sS https://starship.rs/install.sh | sudo sh
+echo "Finished starship"
+read starship
 # Vim Configs
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
